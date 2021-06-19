ed8c7e6a9bd6cfabe829b9a613dfe5df0784947d
TH Huang <tzhsn.huang@gmail.com>
2021-06-19 23:30:30 +0800

    init new website

--- lib/core/constants.dart
+++
@@ -1,6 +0,0 @@
-const AVATAR_PATH = 'assets/images/avatar.png';
-const IMG_GITHUB_URL = 'https://i.imgur.com/K5x2oHp.png';
-const IMG_LINKEDIN_URL = 'https://i.imgur.com/C38zwrQ.png';
-
-const GITHUB_URL = 'https://github.com/thhuang';
-const LINKEDIN_URL = 'https://www.linkedin.com/in/tzuhsuanhuang/';
--- lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'th_app.dart';
-
-void main() => runApp(THApp());
