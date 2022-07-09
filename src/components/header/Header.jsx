5bb648ae5d501355da32f56ee8ccb4190bdf8f2c
TH Huang <tzhsn.huang@gmail.com>
2022-07-09 13:58:01 +0800

    allow header overflow

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import ScrollDown from './ScrollDown';
 
 const Header = () => {
   return (
-    <header className="h-screen pt-24 overflow-hidden">
+    <header className="h-screen pt-24">
       <div className="container-base text-center relative h-full lg:h-4/6 sm:h-screen">
         <h5>Hello I'm</h5>
         <h1>TH Huang</h1>
