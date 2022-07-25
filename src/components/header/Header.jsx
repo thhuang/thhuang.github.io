73d882942a113e5961840c4e9cf79ff05e4aed76
TH Huang <tzhsn.huang@gmail.com>
2022-07-25 15:40:00 +0200

    remove header cta

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './header.css';
-import CTA from './CTA';
 import HeaderSocials from './HeaderSocials';
 import ME from './ME';
 import ScrollDown from './ScrollDown';
@@ -12,7 +11,6 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>TH Huang</h1>
         <h5 className="text-light">Backend Developer</h5>
-        <CTA />
         <HeaderSocials />
         <ME />
         <ScrollDown />
