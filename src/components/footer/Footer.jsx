6e703ef475153b66db0a0e7ee98f1c30af2f8cc7
TH Huang <tzhsn.huang@gmail.com>
2022-07-25 14:08:55 +0200

    add default footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './footer.css';
+import Logo from '../../assets/logo.png';
 
 const Footer = () => {
-  return <div id="footer">Footer</div>;
+  return (
+    <footer id="footer">
+      {/* <a href="#" className="footer__logo"> */}
+      {/*   <img src={Logo} alt="logo" /> */}
+      {/* </a> */}
+    </footer>
+  );
 };
 
 export default Footer;
